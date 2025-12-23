648a4b018e59f8e3f0fdb55b256a5f933567f7c8
Mansi051 <mansiarora1701@gmail.com>
2025-12-23 22:53:16 +0530

    updated training sets

--- model.py
+++ model.py
@@ -4,21 +4,25 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 
 x_train=np.array([
-    [5.0,0],
-    [5.5, 4],
-    [6.0, 3],
-    [6.5, 2],
-    [6.8,1],
-    [7.0,0],
-    [7.2,0],
+    [5.0, 0],
+    [5.5, 2],
+    [6.0, 4],
+    [6.4, 1],
+    [6.8, 3],
+    [7.0, 2],
+    [7.2, 3],
+    [7.5, 4],
+    [7.8, 2],
+    [7.0, 0],
+    [7.1, 1],
     [7.5, 0],
-    [7.8, 1],
-    [8.0, 0],
+    [8.0, 1],
     [8.5, 0],
+    [9.0, 1]
 ])
 
 
-y_train=np.array([0,0,0,0,0,1,1,1,1,1,1])
+y_train=np.array([0,0,0,0,0,0,0,0,0,1,1,1,1,1,1])
 
 scaler=StandardScaler()
 x_scaled=scaler.fit_transform(x_train)
