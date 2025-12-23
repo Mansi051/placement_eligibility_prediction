25ec0857089122fe3fa1ff84ee97533066521557
Mansi051 <mansiarora1701@gmail.com>
2025-12-23 20:45:47 +0530

    Initial commit: FastAPI placement prediction project

---
+++ frontend.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import requests
+
+st.title("Student Placement Prediction")
+
+st.write("Enter your details to check if you are eligible for placement.")
+
+
+cgpa=st.number_input("Enter your CGPA:", min_value=0.0, max_value=10.0, step=0.01)
+backlogs=st.number_input("Number of Backlogs:", min_value=0, step=1)
+
+if st.button("Check Eligibility"):
+    api_url = "https://your-fastapi-url.onrender.com/predict"
+    payload = {"cgpa": cgpa, "backlogs": backlogs}
+    
+    try:
+        response = requests.post(api_url, json=payload)
+        result = response.json()
+        st.success(f"Placement Status: {result['placement_status']}")
+    except Exception as e:
+        st.error(f"Error connecting to API: {e}")
---
+++ model.py
@@ -0,0 +1,25 @@
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+
+x_train=np.array([
+    [5.5, 4],
+    [6.0, 3],
+    [6.5, 2],
+    [6.8,1],
+    [7.0,0],
+    [7.2,0],
+    [7.5, 0],
+    [7.8, 1],
+    [8.0, 0],
+    [8.5, 0],
+])
+
+
+y_train=np.array([0,0,0,0,1,1,1,1,1,1])
+
+scaler=StandardScaler()
+x_scaled=scaler.fit_transform(x_train)
+
+model=LogisticRegression()
+model.fit(x_scaled,y_train)
