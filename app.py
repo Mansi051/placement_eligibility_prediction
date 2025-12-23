0560798f9b634eae5b91645894a90fc1c9588d8b
Mansi051 <mansiarora1701@gmail.com>
2025-12-23 22:38:08 +0530

    deployed on streamlit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 import joblib
-
+import numpy as np
 st.set_page_config(page_title="Placement Predictor", layout="centered")
 
 def load_assets():
@@ -19,12 +19,10 @@ cgpa=st.number_input("Enter your CGPA:", min_value=0.0, max_value=10.0, step=0.0
 backlogs=st.number_input("Number of Backlogs:", min_value=0, step=1)
 
 if st.button("Check Eligibility"):
-    api_url = "https://Mansi051/student-placement-api/predict"
-    payload = {"cgpa": cgpa, "backlogs": backlogs}
-    
-    try:
-        response = requests.post(api_url, json=payload)
-        result = response.json()
-        st.success(f"Placement Status: {result['placement_status']}")
-    except Exception as e:
-        st.error(f"Error connecting to API: {e}")
+    input_data = np.array([[cgpa, backlogs]])
+    prediction = model.predict(input_data)
+
+    if prediction[0] == 1:
+        st.success("✅ Eligible for Placement")
+    else:
+        st.error("❌ Not Eligible for Placement")
